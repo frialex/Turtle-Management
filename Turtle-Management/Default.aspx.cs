da6601c276c88dd4b9205fda36bb833c526965f1
Frison B Alexander <fri.b.alex@gmail.com>
2011-09-14 13:37:13 -0400

    login status from code behind

--- Turtle-Management/Default.aspx.cs
+++ Turtle-Management/Default.aspx.cs
@@ -5,13 +5,16 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using System.Web.Security;
+
 namespace Turtle_Management
 {
     public partial class _Default : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            //string user = User.Identity.Name.ToString();            
+            //bool loggedin = User.Identity.IsAuthenticated;
         }
     }
 }
